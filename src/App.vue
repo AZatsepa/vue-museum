f8bdcd64f4552407404bc33e602e2665b66d61f8
zatsepa <artem.zatsepa@nixsolution.com>
2018-08-20 18:57:27 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <app-navbar></app-navbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AppNavbar from './components/AppNavbar';
+
+export default {
+  name: 'App',
+  components: {
+    AppNavbar,
+  },
+};
+</script>
+
+<style>
+</style>
